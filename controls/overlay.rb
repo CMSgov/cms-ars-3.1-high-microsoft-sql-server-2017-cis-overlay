8d7396f88d9c59d62c90539e05bf80884ffdba5f
karikarshivani <karikarshivani@gmail.com>
2022-05-04 11:26:36 -0400

    Added overlay.rb, inspec.yml and README.md
    Signed-off-by: karikarshivani <karikarshivani@gmail.com>

---
+++ controls/overlay.rb
@@ -0,0 +1,28 @@
+include_controls "microsoft-sql-server-2017-cis-baseline" do
+
+  control "microsoft-sql-server-2017-cis-2.3" do
+    impact 0.0
+    desc "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "microsoft-sql-server-2017-cis-2.9" do
+    impact 0.0
+    desc "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "microsoft-sql-server-2017-cis-3.9" do
+    impact 0.0
+    desc "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "microsoft-sql-server-2017-cis-3.10" do
+    impact 0.0
+    desc "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "microsoft-sql-server-2017-cis-3.11" do
+    impact 0.0
+    desc "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+end
